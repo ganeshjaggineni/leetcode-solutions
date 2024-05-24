1ff4ca798cca49c5b93ff38fe1b99fd709dca6e8
GANESH JAGGINENI <121171965+ganeshjaggineni@users.noreply.github.com>
2024-05-24 15:25:24 +0530

    Time: 0 ms (100.00%) | Memory: 42.6 MB (25.93%) - LeetSync

--- 50-powx-n/powx-n.java
+++ 50-powx-n/powx-n.java
@@ -1,6 +1,27 @@
 class Solution {
     public double myPow(double x, int n) {
         
-        return Math.pow(x,n);
+       // return Math.pow(x,n);
+        long N = n;
+        if(N<0)
+            {
+                N = -N;
+                x = 1/x;
+            }
+        double ans = findPower(x,N);
+      
+     return ans;
     }
+    public  static double findPower(double x,long n)
+    {
+        if(n == 0)
+            return 1;
+        double p = findPower(x,n/2);
+        if((n&1) == 0)
+            return p*p;
+        else 
+            return x*p*p;
+
+    }
+    //BY GANESH JAGGINENI
 }
