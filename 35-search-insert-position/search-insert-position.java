da015f68e47548f2f8dc6c749bdb5f2158538217
GANESH JAGGINENI <121171965+ganeshjaggineni@users.noreply.github.com>
2024-05-24 19:30:32 +0530

    Time: 0 ms (100.00%) | Memory: 43.2 MB (8.74%) - LeetSync

---
+++ 35-search-insert-position/search-insert-position.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int searchInsert(int[] nums, int target) {
+
+       int floor_bin_index =  findFloorBinIndex(nums,target);
+       return floor_bin_index;
+        
+    }
+    public static int findFloorBinIndex(int[] nums,int target)
+    {
+        int low = 0,high = nums.length-1,index = 0;
+        if(target>nums[high])
+            return nums.length;
+        while(low<=high)
+        {
+            int mid = (low+high)/2;
+            if(target<=nums[mid])
+            {
+                high = mid-1;
+                index = mid;
+            }
+            else
+                low = mid+1;
+        }
+        return index;
+    }
+}
