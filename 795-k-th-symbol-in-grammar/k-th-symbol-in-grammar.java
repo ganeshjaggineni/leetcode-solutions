62b4f010ba2d653b9eed4e834418c082e21d0519
GANESH JAGGINENI <121171965+ganeshjaggineni@users.noreply.github.com>
2024-05-24 10:44:57 +0530

    Time: 0 ms (100.00%) | Memory: 40.1 MB (58.51%) - LeetSync

--- 795-k-th-symbol-in-grammar/k-th-symbol-in-grammar.java
+++ 795-k-th-symbol-in-grammar/k-th-symbol-in-grammar.java
@@ -1,13 +1,18 @@
 class Solution {
     public int kthGrammar(int n, int k) {
-        
+
+       //base case 
         if(n==1 && k==1)
             return 0;
-        int len = (int)Math.pow(2,n-1);
-        int mid = len/2;
-        if(k<=mid)
-            return kthGrammar(n-1,k);
+        //recursive case
+       // int len = (int)Math.pow(2,n-1);
+
+        int len = 1<<(n-1); //similar to above commented line to calculate length  a<<i = a*2^i;
+        int mid = len/2; //by checking the k values lies before the mid or after the mid
+        if(k<=mid)  // if it lies before the mid then it is same as above pattern ,otherwise alternate to above pattern
+            return kthGrammar(n-1,k); //the pattern is same as smaller sub probelm
         else
-           return  (kthGrammar(n-1,k-mid))^1;
+           return  (kthGrammar(n-1,k-mid))^1; //the pattern is alternate bit value to same smaller sub problem
     }
+    //BY GANESH JAGGINENI
 }
