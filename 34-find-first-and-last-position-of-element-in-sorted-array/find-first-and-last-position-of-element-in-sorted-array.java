37b3dc45216689d8395a31e6c282a14e0f471045
GANESH JAGGINENI <121171965+ganeshjaggineni@users.noreply.github.com>
2024-05-25 11:13:56 +0530

    Time: 12 ms (20.02%) | Memory: 45.6 MB (86.67%) - LeetSync

---
+++ 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.java
@@ -0,0 +1,80 @@
+class Solution {
+    public int[] searchRange(int[] nums, int target) {
+
+        int first_occurence = floorBinSearch(nums,target);
+        int last_occurence = ceilBinSearch(nums,target);
+
+        return new int[]{first_occurence,last_occurence};
+        
+    }
+    public static int floorBinSearch(int[] nums,int target)
+    {
+        int low = 0;
+        int high = nums.length-1;
+        int first_index = -1;
+        int mid = 0;
+        System.out.println("first at low "+low);
+         System.out.println("first at high ="+high);
+        while(low <= high)
+        {
+            mid = (low+high)/2;
+            if(target == nums[mid])
+            {
+                 first_index = mid;
+                   high = mid-1;
+                   System.out.println("mid = "+mid);
+                       System.out.println("low"+low);
+                   System.out.println("high"+high);
+                   System.out.println("first"+first_index);
+
+            }
+            else if(target < nums[mid])
+            {
+               
+                high = mid - 1;
+                System.out.println("if block");
+                 System.out.println("mid = "+mid);
+                                        System.out.println("low"+low);
+
+                   System.out.println("high"+high);
+               
+            }
+            else
+            {
+                low = mid+1;
+                System.out.println("else block");
+                 System.out.println("mid = "+mid);
+                   System.out.println("low "+low);
+                   System.out.println("high"+high);
+
+                //    System.out.println("first"+first_index);
+            }
+        }
+        return first_index;
+    }
+
+    public static int ceilBinSearch(int[] nums,int target)
+    {
+        int low = 0;
+        int high = nums.length-1;
+        int last_index = -1;
+        int mid = 0;
+        while(low <= high)
+        {
+            mid = (low+high)/2;
+             if(target == nums[mid])
+            {
+                last_index = mid;
+                low = mid+1;
+            }
+            else if(target > nums[mid])
+            {
+              
+                low = mid+1;
+            }
+            else
+                high = mid-1;
+        }
+        return last_index;
+    }
+}
