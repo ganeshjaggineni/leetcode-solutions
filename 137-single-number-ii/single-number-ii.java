85e9ed6d4f788fb3c9af00d5906c88e52574f785
GANESH JAGGINENI <121171965+ganeshjaggineni@users.noreply.github.com>
2024-05-31 11:02:24 +0530

    Time: 3 ms (68.17%) | Memory: 45.1 MB (82.83%) - LeetSync

--- 137-single-number-ii/single-number-ii.java
+++ 137-single-number-ii/single-number-ii.java
@@ -1,20 +1,37 @@
 class Solution {
     public int singleNumber(int[] nums) {
 
-        int ans = 0;
-        for (int i = 0; i < 32; i++) {
-            int sum = 0;
-            for (int j = 0; j < nums.length; j++) {
-                if ((nums[j] & (1 << i)) != 0) {
-                    sum = sum + 1;
-                }
+        // int ans = 0;
+        // for (int i = 0; i < 32; i++) {
+        //     int sum = 0;
+        //     for (int j = 0; j < nums.length; j++) {
+        //         if ((nums[j] & (1 << i)) != 0) {
+        //             sum = sum + 1;
+        //         }
 
+        //     }
+        //     if (sum % 3 != 0)
+        //         ans = ans | (1 << i);
+        // }
+        // return ans;
+
+        int bit_count_arr[] = new int[32];
+
+        for(int num : nums)
+        {
+            for(int i=0;i<32;i++)
+            {
+                if((num & (1<<i)) != 0)
+                    bit_count_arr[i]++;
             }
-            if (sum % 3 != 0)
-                ans = ans | (1 << i);
         }
-        return ans;
-
+        int res = 0;
+        for(int i=0;i<32;i++)
+        {
+            if(bit_count_arr[i]%3 != 0)
+                res |= (1<<i);
+        }
+        return res;
     }
     // BY GANESH JAGGINENI
 
