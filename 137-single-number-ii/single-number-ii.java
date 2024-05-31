a2e1785ef8a2217d039faacb63cfb47ec06eea09
GANESH JAGGINENI <121171965+ganeshjaggineni@users.noreply.github.com>
2024-05-31 10:39:50 +0530

    Time: 4 ms (58.07%) | Memory: 45.5 MB (23.85%) - LeetSync

--- 137-single-number-ii/single-number-ii.java
+++ 137-single-number-ii/single-number-ii.java
@@ -1,29 +1,21 @@
 class Solution {
     public int singleNumber(int[] nums) {
 
-      
         int ans = 0;
-        for(int i=0;i<32;i++)
-        {
-                int sum = 0;
-            for(int j=0;j<nums.length;j++)
-            {
-               if( (nums[j]&(1<<i)) != 0)
-                {
-                    sum = sum+1;
-                    System.out.println("nums[j] ="+nums[j]+"i= "+i);
-                    System.out.println("sum ="+sum);
+        for (int i = 0; i < 32; i++) {
+            int sum = 0;
+            for (int j = 0; j < nums.length; j++) {
+                if ((nums[j] & (1 << i)) != 0) {
+                    sum = sum + 1;
                 }
 
-
             }
-            // System.out.println(sum);
-            if(sum%3 != 0)
-                ans = ans | (1<<i);
+            if (sum % 3 != 0)
+                ans = ans | (1 << i);
         }
         return ans;
-        
+
     }
-    //BY GANESH JAGGINENI
-  
+    // BY GANESH JAGGINENI
+
 }
