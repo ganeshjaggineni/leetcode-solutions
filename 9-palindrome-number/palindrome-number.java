0bb6b30fc268fe7d0e95d836d5cb6926d82f97a0
GANESH JAGGINENI <121171965+ganeshjaggineni@users.noreply.github.com>
2024-05-31 21:56:41 +0530

    Time: 5 ms (73.74%) | Memory: 44 MB (41.90%) - LeetSync

---
+++ 9-palindrome-number/palindrome-number.java
@@ -0,0 +1,18 @@
+class Solution {
+    public boolean isPalindrome(int x) {
+
+        int num = x;
+        int rev_num = 0;
+        while(num > 0)
+        {
+            rev_num = rev_num*10+num%10;
+            num = num/10;
+        }
+        if(x == rev_num)
+            return true;
+
+        return false;
+        
+    }
+    //BY GANESH JAGGINENI
+}
