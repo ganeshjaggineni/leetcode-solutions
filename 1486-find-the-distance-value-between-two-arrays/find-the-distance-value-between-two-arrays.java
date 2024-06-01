f29c6d2a53f8a18c68f43b50c649bfe84ea3d503
GANESH JAGGINENI <121171965+ganeshjaggineni@users.noreply.github.com>
2024-06-01 22:18:27 +0530

    Time: 7 ms (16.91%) | Memory: 44.1 MB (49.04%) - LeetSync

--- 1486-find-the-distance-value-between-two-arrays/find-the-distance-value-between-two-arrays.java
+++ 1486-find-the-distance-value-between-two-arrays/find-the-distance-value-between-two-arrays.java
@@ -1,23 +1,52 @@
 class Solution {
     public int findTheDistanceValue(int[] arr1, int[] arr2, int d) {
 
-        int count = 0;
-        for(int i=0;i<arr1.length;i++)
+        // int count = 0;
+        // for(int i=0;i<arr1.length;i++)
+        // {
+        //     boolean valid = true;
+        //     for(int j=0;j<arr2.length;j++)
+        //     {
+        //         if(Math.abs(arr1[i]-arr2[j]) <= d)
+        //         {
+        //             valid = false;
+        //             break; 
+        //         }
+        //     }
+        //     if(valid)
+        //         count++;
+        // }
+        // return count
+       
+        Arrays.sort(arr2);
+        System.out.println(Arrays.toString(arr2));
+        int dist = 0;
+        for(int num:arr1)
         {
-            boolean valid = true;
-            for(int j=0;j<arr2.length;j++)
-            {
-                if(Math.abs(arr1[i]-arr2[j]) <= d)
-                {
-                    valid = false;
-                    break; 
-                }
-            }
-            if(valid)
-                count++;
+             dist += validBinSearch(num,arr2,d);
         }
-        return count;
+        return dist;
         
+    }
+    public static int validBinSearch(int num,int[] arr2,int d)
+    {
+        int low = 0,high = arr2.length-1;
+        boolean valid = true;
+        while(low <= high)
+        {
+            int mid = (low+high)/2;
+            int diff = Math.abs(num-arr2[mid]);
+            if(diff <= d )
+            {
+                valid = false;
+                break;
+            }
+            else if(num < arr2[mid])
+                high = mid-1;
+            else
+                low = mid+1;
+        }
+        return (valid==true)?1:0;
     }
     //BY GANESH JAGGINENI
 }
