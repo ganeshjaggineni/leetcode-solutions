2adf074a7bd8e1f01750ea3562c6e29179fb086f
GANESH JAGGINENI <121171965+ganeshjaggineni@users.noreply.github.com>
2024-06-03 19:33:25 +0530

    Time: 2 ms (93.91%) | Memory: 43 MB (24.21%) - LeetSync

---
+++ 1468-check-if-n-and-its-double-exist/check-if-n-and-its-double-exist.java
@@ -0,0 +1,16 @@
+class Solution {
+    public boolean checkIfExist(int[] arr) {
+        int len = arr.length;
+        for(int i=0;i<len;i++)
+        {
+            for(int j=0;j<len;j++)
+            {
+                if(arr[i] == 2*arr[j] && i!=j)
+                    return true;
+            }
+        }
+        return false;
+        
+    }
+    //BY GANESH JAGGINENI
+}
