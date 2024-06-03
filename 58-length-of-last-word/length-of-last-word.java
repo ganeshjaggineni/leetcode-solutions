d830b0b4efa604ddbdb657d2853e489f25a91898
GANESH JAGGINENI <121171965+ganeshjaggineni@users.noreply.github.com>
2024-06-03 21:58:46 +0530

    Time: 0 ms (100.00%) | Memory: 41 MB (98.96%) - LeetSync

--- 58-length-of-last-word/length-of-last-word.java
+++ 58-length-of-last-word/length-of-last-word.java
@@ -3,17 +3,15 @@ class Solution {
 
         int count = 0;
         s = s.trim();
-        System.out.println(s);
-        for(int i=s.length()-1;i>=0;i--)
-        {
+        for (int i = s.length() - 1; i >= 0; i--) {
             char ch = s.charAt(i);
-            if(ch== ' ')
+            if (ch == ' ')
                 break;
             else
                 count++;
         }
         return count;
-        
+
     }
-    //BY GANESH JAGGINENI
+    // BY GANESH JAGGINENI
 }
