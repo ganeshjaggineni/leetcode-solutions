1101a90a0fa8d8dfc2a5dbf6630f1e1f6abb8139
GANESH JAGGINENI <121171965+ganeshjaggineni@users.noreply.github.com>
2024-06-05 22:43:06 +0530

    Time: 86 ms (86.35%) | Memory: 48.8 MB (76.38%) - LeetSync

--- 204-count-primes/count-primes.java
+++ 204-count-primes/count-primes.java
@@ -1,20 +1,40 @@
 class Solution {
     public int countPrimes(int n) {
 
-        if (n == 0 || n == 1)
+        // if (n == 0 || n == 1)
+        //     return 0;
+        // boolean[] primes = new boolean[n];
+        // Arrays.fill(primes, true);
+        // primes[0] = true;
+        // primes[1] = true;
+        // int cnt = 0;
+        // for (int i = 2; i < n; i++) {
+        //     if (primes[i]) {
+        //         cnt++;
+        //         for (int j = 2 * i; j < n; j = j + i) {
+        //             if (primes[j]) {
+        //                 primes[j] = false;
+        //             }
+        //         }
+        //     }
+        // }
+        // return cnt;
+
+
+         if (n == 0 || n == 1)
             return 0;
         boolean[] primes = new boolean[n];
         Arrays.fill(primes, true);
         primes[0] = true;
         primes[1] = true;
-        // System.out.println(Arrays.toString(primes));
-        int cnt = 0;
-        for (int i = 2; i < n; i++) {
+        int cnt = n-2;
+        for (int i = 2; i*i<=n; i++) {
             if (primes[i]) {
-                cnt++;
-                for (int j = 2 * i; j < n; j = j + i) {
+                
+                for (int j = i * i; j < n; j = j + i) {
                     if (primes[j]) {
                         primes[j] = false;
+                        cnt--;
                     }
                 }
             }
