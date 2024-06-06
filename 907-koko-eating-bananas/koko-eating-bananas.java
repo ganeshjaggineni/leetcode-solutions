ebd2019638c7485bb404af2826c54c82b619766b
GANESH JAGGINENI <121171965+ganeshjaggineni@users.noreply.github.com>
2024-06-06 11:28:52 +0530

    Time: 22 ms (57.44%) | Memory: 44.7 MB (81.70%) - LeetSync

--- 907-koko-eating-bananas/koko-eating-bananas.java
+++ 907-koko-eating-bananas/koko-eating-bananas.java
@@ -2,41 +2,33 @@ class Solution {
     public int minEatingSpeed(int[] piles, int h) {
         int len = piles.length;
         int max_piles = Integer.MIN_VALUE;
-      for(int i=0;i<len;i++)
-      {
-        if(piles[i] > max_piles)
-            max_piles = piles[i];
-      } 
-    //   System.out.println(max_piles); 
-     int low = 1;
-     int high = max_piles;
-     long res = 0;
-     while(low <= high)
-     {
-     int mid = low+(high-low)/2;
-        long ans = eatBananas(piles,mid);
-        if(ans <= h)
-        {
-           res = mid;
-           high = mid-1;
+        for (int i = 0; i < len; i++) {
+            if (piles[i] > max_piles)
+                max_piles = piles[i];
         }
-        else 
-            low = mid+1;
-       
-     }
-        
-    return (int)res;
+        int low = 1;
+        int high = max_piles;
+        long res = 0;
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+            long ans = eatBananas(piles, mid);
+            if (ans <= h) {
+                res = mid;
+                high = mid - 1;
+            } else
+                low = mid + 1;
+
+        }
+
+        return (int) res;
     }
-    public static long eatBananas(int[] piles,int k)
-    {
+
+    public static long eatBananas(int[] piles, int k) {
         long value = 0;
-        for(int i=0;i<piles.length;i++)
-        {
-            // System.out.println("ceil ="+(int)Math.ceil((double)30/4));
-            value +=  (int)Math.ceil((double)piles[i]/k);
+        for (int i = 0; i < piles.length; i++) {
+            value += (int) Math.ceil((double) piles[i] / k);
         }
-            System.out.println("for k ="+k+" value = "+value);
-    return value;
+        return value;
     }
-    //BY GANESH JAGGINENI
+    // BY GANESH JAGGINENI
 }
