8990e2c90643f30a4003770b5b8d9950d0da81cc
GANESH JAGGINENI <121171965+ganeshjaggineni@users.noreply.github.com>
2024-06-06 10:02:29 +0530

    Time: 2 ms (5.78%) | Memory: 42 MB (55.56%) - LeetSync

--- 74-search-a-2d-matrix/search-a-2d-matrix.java
+++ 74-search-a-2d-matrix/search-a-2d-matrix.java
@@ -1,29 +1,50 @@
 class Solution {
     public boolean searchMatrix(int[][] matrix, int target) {
-   
-        for(int i=0;i<matrix.length;i++)
-        {
-            for(int j=0;j<matrix[0].length;j++)
-            {
-                if(matrix[i][j] == target)
-                   return true;
-            }
 
+        // for(int i=0;i<matrix.length;i++)
+        // {
+        // for(int j=0;j<matrix[0].length;j++)
+        // {
+        // if(matrix[i][j] == target)
+        // return true;
+        // }
+
+        // }
+        // return false;
+
+        for (int[] arr : matrix) {
+            int len = arr.length;
+            if (arr[0] <= target && arr[len - 1] >= target) {
+                boolean res = SearchTarget(arr, target);
+                if (res)
+                    return true;
+
+            }
         }
         return false;
-   
-   
-   
-   
-    //     for(int[] arr:matrix)
-    //     {
-    //         boolen res = SearchTarget(arr,target);
-    //     }
 
-    // }
-    // public static boolean SearchTarget(int[] arr,int target)
-    // {
-
-    // }
     }
+
+    public static boolean SearchTarget(int[] arr, int target) {
+        int len = arr.length;
+        int low = 0;
+        System.out.println(Arrays.toString(arr));
+        int high = len - 1;
+        while (low <= high) {
+            while (low <= high) {
+                int mid = low + (high - low) / 2;
+                if (arr[mid] == target) {
+                    return true;
+                } else if (arr[mid] > target)
+                    high = mid - 1;
+                else
+                    low = mid + 1;
+
+            }
+        }
+        return false;
+
+    }
+    // BY GANESH JAGGINENI
+
 }
