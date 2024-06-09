c004c408eac67d5de25a897faaf4fa31f4f2a652
GANESH JAGGINENI <121171965+ganeshjaggineni@users.noreply.github.com>
2024-06-09 20:55:37 +0530

    Time: 3 ms (72.95%) | Memory: 42.7 MB (77.93%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.java
+++ 125-valid-palindrome/valid-palindrome.java
@@ -1,21 +1,17 @@
 class Solution {
     public boolean isPalindrome(String s) {
         s = s.toLowerCase();
-        System.out.println(s);
+        int p1 = 0;
+        int p2 = s.length() - 1;
 
-    //    if( s.charAt(p1) >= 'a' && s.charAt(p1)<='z')
-           int  p1 = 0;
-            int p2 = s.length()-1;
+        while (p1 < p2) {
 
-        while(p1<p2)
-        {
-
-           while(p1<p2 && !Character.isLetterOrDigit(s.charAt(p1)))
+            while (p1 < p2 && !Character.isLetterOrDigit(s.charAt(p1)))
                 p1++;
-            while(p1<p2 && !Character.isLetterOrDigit(s.charAt(p2)))
+            while (p1 < p2 && !Character.isLetterOrDigit(s.charAt(p2)))
                 p2--;
 
-            if(s.charAt(p1) != s.charAt(p2))
+            if (s.charAt(p1) != s.charAt(p2))
                 return false;
 
             p1++;
@@ -23,7 +19,7 @@ class Solution {
         }
 
         return true;
-        
+
     }
-    //BY GANESH JAGGINENI
+    // BY GANESH JAGGINENI
 }
