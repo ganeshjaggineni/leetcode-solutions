740481347a3db25d6e7b0b1806c86e78b2ac3b03
GANESH JAGGINENI <121171965+ganeshjaggineni@users.noreply.github.com>
2024-06-10 22:29:37 +0530

    Time: 25 ms (5.42%) | Memory: 56 MB (25.16%) - LeetSync

--- 238-product-of-array-except-self/product-of-array-except-self.java
+++ 238-product-of-array-except-self/product-of-array-except-self.java
@@ -2,6 +2,7 @@
 class Solution {
     public int[] productExceptSelf(int[] nums) {
 
+        /*
         int n = nums.length;
         int[] ans = new int[n];
         int prod = 1;
@@ -27,6 +28,34 @@ class Solution {
         }
 
         return ans;
+        */
+
+
+        //prefix product and suffix product method
+        //prefix product calculation
+        int n = nums.length;
+        int[] pre = new int[n];
+        int[] suf = new int[n];
+        int[] ans = new int[n];
+        pre[0] = nums[0];
+        for(int i=1;i<n;i++)
+        {
+            pre[i] = pre[i-1]*nums[i];
+        }
+        suf[n-1] = nums[n-1];
+         for(int i=n-2;i>=0;i--)
+        {
+            suf[i] = suf[i+1]*nums[i];
+        }
+        System.out.println(Arrays.toString(pre));
+         System.out.println(Arrays.toString(suf));
+         ans[0] = suf[1];
+         ans[n-1] = pre[n-2];
+         for(int i=1;i<n-1;i++)
+         {
+            ans[i] = pre[i-1]*suf[i+1];
+         }
+         return ans;
     }
     //BY GANESH JAGGINENI
 }
