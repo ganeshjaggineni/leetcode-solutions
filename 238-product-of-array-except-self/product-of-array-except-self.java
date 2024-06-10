2d0964866787110bd4a36eab85d28ba4f07d2cea
GANESH JAGGINENI <121171965+ganeshjaggineni@users.noreply.github.com>
2024-06-10 22:14:39 +0530

    Time: 2 ms (89.04%) | Memory: 54.9 MB (90.44%) - LeetSync

--- 238-product-of-array-except-self/product-of-array-except-self.java
+++ 238-product-of-array-except-self/product-of-array-except-self.java
@@ -1,3 +1,4 @@
+
 class Solution {
     public int[] productExceptSelf(int[] nums) {
 
@@ -5,40 +6,27 @@ class Solution {
         int[] ans = new int[n];
         int prod = 1;
         int zero_count = 0;
-        int zc_idx = 0;
-        boolean found = false;
-        for(int i=0;i<n;i++)
-        { 
-                    if(nums[i] != 0)
-                             prod = prod*nums[i];
-                    if(nums[i] == 0)
-                        zero_count++;
+        int zero_idx = -1;
+        for (int i = 0; i < n; i++) {
+            if (nums[i] != 0)
+                prod = prod * nums[i];
+            else {
+                zero_count++;
+                zero_idx = i;
+            }
 
-                    if(!found)
-                    {
-                    if(zero_count == 1)
-                    {
-                        zc_idx = i;
-                        found = true;
-                        System.out.println(zc_idx);
-                    }
-                    }
         }
-        if(zero_count  == 1)
-            ans[zc_idx] = prod;
-        else if(zero_count > 1)
-        {
+        if (zero_count == 1)
+            ans[zero_idx] = prod;
+        else if (zero_count > 1) {
             return ans;
+        } else {
+            for (int i = 0; i < n; i++) {
+                ans[i] = prod / nums[i];
+            }
         }
-        else
-        {
-         for(int i=0;i<n;i++)
-        { 
-                   ans[i] = prod/nums[i];
-        }
-        }
-        
-        
+
         return ans;
     }
+    //BY GANESH JAGGINENI
 }
