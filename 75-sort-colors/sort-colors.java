238ad84ea6b2937ace430c16730a03a1a49fcfb1
GANESH JAGGINENI <121171965+ganeshjaggineni@users.noreply.github.com>
2024-06-12 11:01:59 +0530

    Time: 0 ms (100.00%) | Memory: 41.4 MB (94.55%) - LeetSync

--- 75-sort-colors/sort-colors.java
+++ 75-sort-colors/sort-colors.java
@@ -1,26 +1,55 @@
 class Solution {
     public void sortColors(int[] nums) {
 
-        int zeros_count = 0, ones_count = 0, twos_count = 0;
-        int len = nums.length;
-        for (int i = 0; i < len; i++) {
-            if (nums[i] == 0)
-                zeros_count++;
-            else if (nums[i] == 1)
-                ones_count++;
-            else if (nums[i] == 2)
-                twos_count++;
+        // DUTCH NATIONAL FLAG ALGORITHM
+        int low = 0;
+        int mid = 0;
+        int high = nums.length - 1;
+
+        while (mid <= high) {
+            if (nums[mid] == 0) {
+                // swap a[low],a[mid]
+                int temp = nums[low];
+                nums[low] = nums[mid];
+                nums[mid] = temp;
+                mid++;
+                low++;
+            } else if (nums[mid] == 1)
+                mid++;
+            else // a[mid] == 2
+            {
+                // swap a[mid],a[high]
+                int temp = nums[high];
+                nums[high] = nums[mid];
+                nums[mid] = temp;
+                high--;
+            }
         }
 
-        int i = 0;
-        while (zeros_count-- > 0)
-            nums[i++] = 0;
-        while (ones_count-- > 0)
-            nums[i++] = 1;
-        while (twos_count-- > 0)
-            nums[i++] = 2;
-
-      
+        /*
+         * //two pass algorithm
+         * int zeros_count = 0, ones_count = 0, twos_count = 0;
+         * int len = nums.length;
+         * //FIRST PASS : count the number of 0's,1's,2's
+         * for (int i = 0; i < len; i++) {
+         * if (nums[i] == 0)
+         * zeros_count++;
+         * else if (nums[i] == 1)
+         * ones_count++;
+         * else if (nums[i] == 2)
+         * twos_count++;
+         * }
+         * //SECOND PASS : overwrite the array with counted 0's,1's and 2's count
+         * int i = 0;
+         * while (zeros_count-- > 0)
+         * nums[i++] = 0;
+         * while (ones_count-- > 0)
+         * nums[i++] = 1;
+         * while (twos_count-- > 0)
+         * nums[i++] = 2;
+         * 
+         * 
+         */
 
     }
     // BY GANESH JAGGINENI
