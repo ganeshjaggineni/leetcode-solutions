850d989c86d00f83095db94441bf7ea3040b66df
GANESH JAGGINENI <121171965+ganeshjaggineni@users.noreply.github.com>
2024-06-12 10:03:30 +0530

    Time: 0 ms (100.00%) | Memory: 42 MB (16.44%) - LeetSync

--- 75-sort-colors/sort-colors.java
+++ 75-sort-colors/sort-colors.java
@@ -6,9 +6,9 @@ class Solution {
         for (int i = 0; i < len; i++) {
             if (nums[i] == 0)
                 zeros_count++;
-            if (nums[i] == 1)
+            else if (nums[i] == 1)
                 ones_count++;
-            if (nums[i] == 2)
+            else if (nums[i] == 2)
                 twos_count++;
         }
 
