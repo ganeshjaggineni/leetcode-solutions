6591acc9dc0878a07df5bbdaef6c6607913b0134
GANESH JAGGINENI <121171965+ganeshjaggineni@users.noreply.github.com>
2024-06-22 12:45:03 +0530

    Time: 5 ms (86.33%) | Memory: 44.6 MB (37.27%) - LeetSync

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.java
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.java
@@ -1,27 +1,52 @@
 class Solution {
     public int lengthOfLongestSubstring(String s) {
 
-        int left = 0;
-        int right = 0;
-        int longest_substring = 0;
-        int n = s.length();
-        Set<Character> set = new HashSet<>();
-        while(right < n)
+        // int left = 0;
+        // int right = 0;
+        // int longest_substring = 0;
+        // int n = s.length();
+        // Set<Character> set = new HashSet<>();
+        // while(right < n)
+        // {
+        //     char ch = s.charAt(right);
+        //     if(!set.contains(ch))
+        //     {
+        //         set.add(ch);
+        //         longest_substring = Math.max(longest_substring,right-left+1);
+        //         right++;
+        //     }
+        //     else
+        //     {
+        //         set.remove(s.charAt(left));
+        //         left++;
+        //     }
+        // }
+        // return longest_substring;
+
+
+        //METHOD 2 USING HASHMAP
+        if(s.length() == 1)
+            return 1;
+        int start = 0;
+        int end = 0;
+        Map<Character,Integer> mapind = new HashMap<>();
+        int maxlen = 0;
+        while(end < s.length())
         {
-            char ch = s.charAt(right);
-            if(!set.contains(ch))
+            char ch = s.charAt(end);
+            if(mapind.containsKey(ch) && mapind.get(ch) >= start)
             {
-                set.add(ch);
-                longest_substring = Math.max(longest_substring,right-left+1);
-                right++;
+                start = mapind.get(ch)+1;
             }
-            else
-            {
-                set.remove(s.charAt(left));
-                left++;
-            }
+
+
+            mapind.put(ch,end);
+            maxlen = Math.max(maxlen,end-start+1);
+
+         end++;
         }
-        return longest_substring;
+        return maxlen;
+
         
     }
     //BY GANESH JAGGINENI
