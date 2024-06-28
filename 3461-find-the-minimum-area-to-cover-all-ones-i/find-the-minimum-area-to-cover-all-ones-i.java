c6e6f157877910e8ea0bf270a26ab4bf9f7eeac8
GANESH JAGGINENI <121171965+ganeshjaggineni@users.noreply.github.com>
2024-06-28 07:34:48 +0530

    Time: 5 ms (94.48%) | Memory: 196.8 MB (45.70%) - LeetSync

--- 3461-find-the-minimum-area-to-cover-all-ones-i/find-the-minimum-area-to-cover-all-ones-i.java
+++ 3461-find-the-minimum-area-to-cover-all-ones-i/find-the-minimum-area-to-cover-all-ones-i.java
@@ -1,67 +1,93 @@
 class Solution {
     public int minimumArea(int[][] grid) {
-        int len_start_point = -1, len_end_point = -1;
-        int breadth_start_point = -1, breadth_end_point = -1;
+
+        /*
+         * 
+         * int len_start_point = -1, len_end_point = -1;
+         * int breadth_start_point = -1, breadth_end_point = -1;
+         * int row_len = grid.length;
+         * int col_len = grid[0].length;
+         * 
+         * // Find len_start_point
+         * for (int i = 0; i < row_len; i++) {
+         * boolean found = false;
+         * for (int j = 0; j < col_len; j++) {
+         * if (grid[i][j] == 1) {
+         * len_start_point = i;
+         * found = true;
+         * break;
+         * }
+         * }
+         * if (found)
+         * break;
+         * }
+         * 
+         * // Find len_end_point
+         * for (int i = row_len - 1; i >= 0; i--) {
+         * boolean found = false;
+         * for (int j = col_len - 1; j >= 0; j--) {
+         * if (grid[i][j] == 1) {
+         * len_end_point = i;
+         * found = true;
+         * break;
+         * }
+         * }
+         * if (found)
+         * break;
+         * }
+         * 
+         * // Find breadth_start_point
+         * for (int j = 0; j < col_len; j++) {
+         * boolean found = false;
+         * for (int i = 0; i < row_len; i++) {
+         * if (grid[i][j] == 1) {
+         * breadth_start_point = j;
+         * found = true;
+         * break;
+         * }
+         * }
+         * if (found)
+         * break;
+         * }
+         * 
+         * // Find breadth_end_point
+         * for (int j = col_len - 1; j >= 0; j--) {
+         * boolean found = false;
+         * for (int i = row_len - 1; i >= 0; i--) {
+         * if (grid[i][j] == 1) {
+         * breadth_end_point = j;
+         * found = true;
+         * break;
+         * }
+         * }
+         * if (found)
+         * break;
+         * }
+         * 
+         * return (len_end_point - len_start_point + 1) * (breadth_end_point -
+         * breadth_start_point + 1);
+         */
+
+        // METHOD 2
         int row_len = grid.length;
         int col_len = grid[0].length;
-        
-        // Find len_start_point
+        int top = row_len;
+        int bottom = -1;
+        int left = col_len;
+        int right = -1;
+
         for (int i = 0; i < row_len; i++) {
-            boolean found = false;
             for (int j = 0; j < col_len; j++) {
                 if (grid[i][j] == 1) {
-                    len_start_point = i;
-                    found = true;
-                    break;
+                    top = Math.min(top, i);
+                    bottom = Math.max(bottom, i);
+                    left = Math.min(left, j);
+                    right = Math.max(right, j);
                 }
             }
-            if (found)
-                break;
         }
-        
-        // Find len_end_point
-        for (int i = row_len - 1; i >= 0; i--) {
-            boolean found = false;
-            for (int j = col_len - 1; j >= 0; j--) {
-                if (grid[i][j] == 1) {
-                    len_end_point = i;
-                    found = true;
-                    break;
-                }
-            }
-            if (found)
-                break;
-        }
-        
-        // Find breadth_start_point
-        for (int j = 0; j < col_len; j++) {
-            boolean found = false;
-            for (int i = 0; i < row_len; i++) {
-                if (grid[i][j] == 1) {
-                    breadth_start_point = j;
-                    found = true;
-                    break;
-                }
-            }
-            if (found)
-                break;
-        }
-        
-        // Find breadth_end_point
-        for (int j = col_len - 1; j >= 0; j--) {
-            boolean found = false;
-            for (int i = row_len - 1; i >= 0; i--) {
-                if (grid[i][j] == 1) {
-                    breadth_end_point = j;
-                    found = true;
-                    break;
-                }
-            }
-            if (found)
-                break;
-        }
-        
-        return (len_end_point - len_start_point + 1) * (breadth_end_point - breadth_start_point + 1);
+        return (bottom - top + 1) * (right - left + 1);
+
     }
-    //BY GANESH JAGGINENI
+    // BY GANESH JAGGINENI
 }
