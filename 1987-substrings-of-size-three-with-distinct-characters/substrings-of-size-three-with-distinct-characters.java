88a2e7e0a1783490bde3c7caaf7738a8fbbb8afa
GANESH JAGGINENI <121171965+ganeshjaggineni@users.noreply.github.com>
2024-06-28 09:52:33 +0530

    Time: 1 ms (90.13%) | Memory: 41.1 MB (89.67%) - LeetSync

---
+++ 1987-substrings-of-size-three-with-distinct-characters/substrings-of-size-three-with-distinct-characters.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int countGoodSubstrings(String s) {
+
+        int n = s.length();
+        int count = 0;
+        for(int i=0;i<n-2;i++)
+        {
+            if(s.charAt(i) != s.charAt(i+1) && s.charAt(i+1) != s.charAt(i+2) &&s.charAt(i) != s.charAt(i+2))
+            {
+                count++;
+            }
+        }
+        return count;
+        
+    }
+    //BY GANESH JAGGINENI
+}
