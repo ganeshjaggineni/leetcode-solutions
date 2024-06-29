33427b2c8e08d1cfc41bd068ad95cfb6e6cb3092
GANESH JAGGINENI <121171965+ganeshjaggineni@users.noreply.github.com>
2024-06-29 08:49:56 +0530

    Time: 78 ms (5.05%) | Memory: 45.1 MB (13.36%) - LeetSync

--- 3349-maximum-length-substring-with-two-occurrences/maximum-length-substring-with-two-occurrences.java
+++ 3349-maximum-length-substring-with-two-occurrences/maximum-length-substring-with-two-occurrences.java
@@ -1,6 +1,7 @@
 class Solution {
     public int maximumLengthSubstring(String s) {
-        
+
+      /*  
         int maxlen = 0;
         int n = s.length();
         for(int i=0;i<n;i++)
@@ -26,5 +27,40 @@ class Solution {
                 return false;
         }
         return true;
+        */
+
+        //METHOD 2 SLIDING WINDOW
+
+        int left = 0;
+        int right = 0;
+        int n = s.length();
+        int max_substr_len = 0;
+        Map<Character,Integer> charcount = new HashMap<>();
+
+        while(right < n)
+        {
+            char ch = s.charAt(right);
+            System.out.println("char at right = "+ch);
+            charcount.put(ch,charcount.getOrDefault(ch,0)+1);
+            if(charcount.get(ch) > 2)
+            {
+                System.out.println("char count > 2 "+ch);
+                while(charcount.get(ch) > 2)
+                {
+                    System.out.println("in while");
+                    charcount.put(s.charAt(left),charcount.get(s.charAt(left))-1);
+                    if(charcount.get(s.charAt(left)) == 0)
+                        charcount.remove(s.charAt(left));
+                    left++;
+                    System.out.println("left pointer "+left);
+                }
+
+                System.out.println("max sub str len = "+max_substr_len);
+                System.out.println("right pointer before = "+right);
+            }
+                max_substr_len = Math.max(max_substr_len,right-left+1);
+                right++;
+        }
+        return max_substr_len;
     }
 }
