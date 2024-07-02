31109554a4c77f9cb26277e8130e096025516fc9
GANESH JAGGINENI <121171965+ganeshjaggineni@users.noreply.github.com>
2024-07-02 06:51:29 +0530

    Time: 3 ms (90.95%) | Memory: 42.8 MB (85.37%) - LeetSync

--- 350-intersection-of-two-arrays-ii/intersection-of-two-arrays-ii.java
+++ 350-intersection-of-two-arrays-ii/intersection-of-two-arrays-ii.java
@@ -1,6 +1,8 @@
 class Solution {
     public int[] intersect(int[] nums1, int[] nums2) {
-        
+
+       //METHOD 1 BRUTEFORCE
+       /* 
         List<Integer> res = new ArrayList<>();
         for(int i=0;i<nums1.length;i++)
         {
@@ -20,6 +22,38 @@ class Solution {
             ans[i] = res.get(i);
         }
         return ans;
+        */
+
+        //METHOD 2 TWO POINTERS
+
+        int p1 = 0;
+        int p2 = 0;
+        List<Integer> res = new ArrayList<>();
+        Arrays.sort(nums1);
+        Arrays.sort(nums2);
+
+        while(p1 < nums1.length && p2 <nums2.length)
+        {
+            if(nums1[p1] == nums2[p2])
+            {
+                res.add(nums1[p1]);
+                p1++;
+                p2++;
+            }
+            else if(nums1[p1] < nums2[p2])
+            {
+                p1++;
+            }
+            else
+                p2++;
+        }
+         int[] intersection = new int[res.size()];
+        for(int i=0;i<res.size();i++)
+        {
+            intersection[i] = res.get(i);
+        }
+        return intersection;
+
     }
     //BY GANESH JAGGINENI
 }
