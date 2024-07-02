5882960169d1361eee3cbc50bd6c707be5d25e30
GANESH JAGGINENI <121171965+ganeshjaggineni@users.noreply.github.com>
2024-07-03 00:27:22 +0530

    Time: 1 ms (74.10%) | Memory: 40.8 MB (23.34%) - LeetSync

--- 2571-find-the-pivot-integer/find-the-pivot-integer.java
+++ 2571-find-the-pivot-integer/find-the-pivot-integer.java
@@ -1,28 +1,45 @@
 class Solution {
     public int pivotInteger(int n) {
         
-        int pivot_before_sum = 0;
-        int pivot_after_sum = 0;
-        for(int i=1;i<=n;i++)
-        {
-            pivot_before_sum = findSum(1,i);
-            pivot_after_sum = findSum(i,n);
-            if(pivot_before_sum == pivot_after_sum)
-            {
-                return i;
-            }
-        }
-        return -1;
+    //     int pivot_before_sum = 0;
+    //     int pivot_after_sum = 0;
+    //     for(int i=1;i<=n;i++)
+    //     {
+    //         pivot_before_sum = findSum(1,i);
+    //         pivot_after_sum = findSum(i,n);
+    //         if(pivot_before_sum == pivot_after_sum)
+    //         {
+    //             return i;
+    //         }
+    //     }
+    //     return -1;
 
-    }
-    private static int findSum(int start ,int pivot)
+    // }
+    // private static int findSum(int start ,int pivot)
+    // {
+    //     int sum = 0;
+    //     for(int i=start;i<=pivot;i++)
+    //     {
+    //         sum = sum+i;
+    //     }
+    //     return sum;
+
+    //METHOD 2 PREFIX SUM
+    int sum = 0;
+    int[] sum_array = new int[n+1];
+    sum_array[0] =0;
+    for(int i=1;i<=n;i++)
     {
-        int sum = 0;
-        for(int i=start;i<=pivot;i++)
+        sum_array[i] = sum_array[i-1]+i;
+    }
+    for(int i=1;i<=n;i++)
+    {
+        if(sum_array[i] == sum_array[n]-sum_array[i-1])
         {
-            sum = sum+i;
+            return i;
         }
-        return sum;
+    }
+    return -1;
     }   
     //  BY GANESH JAGGINENI
 }
