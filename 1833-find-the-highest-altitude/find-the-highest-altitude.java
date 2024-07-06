e03ad0f11cc25243973de925bc5ac999cf0a56a1
GANESH JAGGINENI <121171965+ganeshjaggineni@users.noreply.github.com>
2024-07-06 22:51:07 +0530

    Time: 0 ms (100.00%) | Memory: 41.1 MB (43.79%) - LeetSync

--- 1833-find-the-highest-altitude/find-the-highest-altitude.java
+++ 1833-find-the-highest-altitude/find-the-highest-altitude.java
@@ -1,6 +1,7 @@
 class Solution {
     public int largestAltitude(int[] gain) {
 
+        /*
         int n = gain.length;
         int[] prefix_height = new int[n+1];
         prefix_height[0] = 0;
@@ -12,6 +13,18 @@ class Solution {
         Arrays.sort(prefix_height);
         // System.out.println(Arrays.toString(prefix_height));
         return prefix_height[n];
+
+        */
+
+        int n = gain.length;
+        int max_alti = 0;
+        int alti = 0;
+        for(int i=0;i<n;i++)
+        {
+            alti = alti+gain[i];
+            max_alti = Math.max(alti,max_alti);
+        }
+        return max_alti;
         
     }
 }
