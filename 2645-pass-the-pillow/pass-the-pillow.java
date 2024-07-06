1a4b43dd7a3f91a57beb5bae0d53282e6faca23c
GANESH JAGGINENI <121171965+ganeshjaggineni@users.noreply.github.com>
2024-07-06 10:16:16 +0530

    Time: 0 ms (100.00%) | Memory: 40.5 MB (24.58%) - LeetSync

--- 2645-pass-the-pillow/pass-the-pillow.java
+++ 2645-pass-the-pillow/pass-the-pillow.java
@@ -1,31 +1,48 @@
 class Solution {
     public int passThePillow(int n, int time) {
 
-        boolean right = true;
-        boolean left = true;
-        int var = 1;
-        while(time-- > 0)
-        {
-            if(right)
-            {
-             var++;
-            }
-            else if(left)
-            {
-                var--;
-            }
-            if(var == n)
-            {
-                right = false;
-                left = true;
-            }
-            if(var == 1)
-            {
-                left = false;
-                right = true;
+        // boolean right = true;
+        // boolean left = true;
+        // int var = 1;
+        // while(time-- > 0)
+        // {
+        //     if(right)
+        //     {
+        //      var++;
+        //     }
+        //     else if(left)
+        //     {
+        //         var--;
+        //     }
+        //     if(var == n)
+        //     {
+        //         right = false;
+        //         left = true;
+        //     }
+        //     if(var == 1)
+        //     {
+        //         left = false;
+        //         right = true;
+        //     }
+        // }
+        // return var;
+          int position = 1;
+        boolean forward = true;
+
+        for (int t = 1; t <= time; t++) {
+            if (forward) {
+                position++;
+                if (position == n) {
+                    forward = false;
+                }
+            } else {
+                position--;
+                if (position == 1) {
+                    forward = true;
+                }
             }
         }
-        return var;
-        
+
+        return position;
     }
 }
