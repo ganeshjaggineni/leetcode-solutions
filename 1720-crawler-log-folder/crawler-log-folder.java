bb88793cd0dee817d191cf994ad9e883c04d2681
GANESH JAGGINENI <121171965+ganeshjaggineni@users.noreply.github.com>
2024-07-10 21:39:20 +0530

    Time: 1 ms (99.42%) | Memory: 41.8 MB (81.60%) - LeetSync

--- 1720-crawler-log-folder/crawler-log-folder.java
+++ 1720-crawler-log-folder/crawler-log-folder.java
@@ -1,6 +1,33 @@
 class Solution {
     public int minOperations(String[] logs) {
+//METHOD 1 : USING STACK
+/*
+        Stack<Integer> st = new Stack<>();
+        int temp = 0;
+        st.add(temp);
+        for(int i=0;i<logs.length;i++)
+        {
+            if(logs[i].equals("../"))
+            {
+                if(st.peek() != 0)
+                {
+                st.pop();
+                temp--;
+                }
+            }
+            else if(logs[i].equals("./"))
+                continue;
+            else
+            {
+                temp = temp+1;
+                st.add(temp);
+            }
+        }
 
+        return st.peek();
+        */
+
+//METHOD 2 : WITHOUT USING STACK
         int depth = 0;
         for (int i = 0; i < logs.length; i++) {
             if (logs[i].equals("../")) {
