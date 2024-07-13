2fb77429691dbefd3d73420ec06e16ef6cb020cc
GANESH JAGGINENI <121171965+ganeshjaggineni@users.noreply.github.com>
2024-07-14 00:36:27 +0530

    Time: 1 ms (76.18%) | Memory: 41.5 MB (78.44%) - LeetSync

---
+++ 1944-truncate-sentence/truncate-sentence.java
@@ -0,0 +1,22 @@
+class Solution {
+    public String truncateSentence(String s, int k) {
+        
+        int end_idx = 0;
+        for(int i=0;i<s.length();i++)
+        {
+            char ch = s.charAt(i);
+            if(ch == ' ')
+            {
+                k--;
+                if(k == 0)
+                {
+                    end_idx = i;
+                }
+            }
+        }
+        if(k > 0)
+            return s;
+        else
+            return s.substring(0,end_idx);
+    }
+}
