897415b9a4490204833ef49f30918a194898a322
GANESH JAGGINENI <121171965+ganeshjaggineni@users.noreply.github.com>
2024-07-13 12:16:46 +0530

    Time: 0 ms (100.00%) | Memory: 41.9 MB (5.39%) - LeetSync

--- 144-binary-tree-preorder-traversal/binary-tree-preorder-traversal.java
+++ 144-binary-tree-preorder-traversal/binary-tree-preorder-traversal.java
@@ -15,11 +15,33 @@
  */
 class Solution {
     public List<Integer> preorderTraversal(TreeNode root) {
+
+        //method 1 USING RECURSION
+        /*
         List<Integer> ans = new ArrayList<>();
         preorder(root,ans);
         return ans;
-        
+        */
+
+        //METHOD 2 iterative way using stack
+        Stack<TreeNode> treestack = new Stack<>();
+        List<Integer> res = new ArrayList<>();
+        treestack.add(root);
+        if(root == null)
+            return res;
+        while(!treestack.isEmpty())
+        {
+            TreeNode top = treestack.pop();
+            if(top != null)
+                res.add(top.val);
+            if(top.right != null)
+                treestack.add(top.right);
+            if(top.left != null)
+                treestack.add(top.left);
+        }
+        return res;
     }
+    /*
     private static void preorder(TreeNode root,List<Integer> ans)
     {
         if(root == null)
@@ -28,4 +50,5 @@ class Solution {
         preorder(root.left,ans);
         preorder(root.right,ans);
     }
+    */
 }
