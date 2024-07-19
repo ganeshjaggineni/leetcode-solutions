dc0d9af359ec028653277cf5f243fc25dd77622f
GANESH JAGGINENI <121171965+ganeshjaggineni@users.noreply.github.com>
2024-07-19 21:38:35 +0530

    Time: 6 ms (9.24%) | Memory: 44.9 MB (82.41%) - LeetSync

---
+++ 1496-lucky-numbers-in-a-matrix/lucky-numbers-in-a-matrix.java
@@ -0,0 +1,46 @@
+class Solution {
+    public List<Integer> luckyNumbers (int[][] matrix) {
+
+        List<Integer> list = new ArrayList<>();
+        Set<Integer> set1 = new HashSet<>();
+        for(int i=0;i<matrix.length;i++)
+        {
+            int min = Integer.MAX_VALUE;
+            for(int j=0;j<matrix[0].length;j++)
+            {
+                if(matrix[i][j]< min)
+                    {
+                        min = matrix[i][j];
+                    }
+            }
+            set1.add(min);
+        }
+        System.out.println(set1);
+
+            for(int j=0;j<matrix[0].length;j++)
+            {
+               int max = Integer.MIN_VALUE;
+             for(int i=0;i<matrix.length;i++)
+            {
+                 if(matrix[i][j] > max)
+                {
+                   
+                    max = matrix[i][j];
+                    // System.out.println(max);
+                }
+            }
+            if(set1.contains(max))
+            {
+
+                System.out.println(max);
+                list.add(max);
+            }
+
+            
+
+         }
+           
+        
+    return list;   
+    }
+}
