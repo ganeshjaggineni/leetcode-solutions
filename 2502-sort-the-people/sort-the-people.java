80c5261500b0113a3380ca5341d02e05fe820ae8
GANESH JAGGINENI <121171965+ganeshjaggineni@users.noreply.github.com>
2024-07-22 21:53:18 +0530

    Time: 7 ms (90.64%) | Memory: 45.6 MB (5.32%) - LeetSync

--- 2502-sort-the-people/sort-the-people.java
+++ 2502-sort-the-people/sort-the-people.java
@@ -1,9 +1,10 @@
 class Solution {
     public String[] sortPeople(String[] names, int[] heights) {
+        /*  METHOD 1 USING BUBBLE SORT
         int n = heights.length;
         for(int i=0;i<n-1;i++)
         {
-            for(int j=0;j<n-1;j++)
+            for(int j=0;j<n-1-i;j++)
             {
                 if(heights[j] < heights[j+1])
                 {
@@ -20,6 +21,24 @@ class Solution {
         }
         // System.out.println(Arrays.toString(heights));
         return names;
+
+        */
+        //METHOD 2 USING HASHMAP & SORTING
+
+        Map<Integer,String> htToNamesMap = new HashMap<>();
+        int people_cnt = names.length;
+        for(int i=0;i<people_cnt;i++)
+        {
+            htToNamesMap.put(heights[i],names[i]);
+        }
+        Arrays.sort(heights);
+        String[] sortedNames = new String[people_cnt];
+
+        for(int i=people_cnt-1;i>=0;i--)
+        {
+            sortedNames[people_cnt-i-1] = htToNamesMap.get(heights[i]);
+        }
+        return sortedNames;
     }
     //BY GANESH JAGGINENI
 }
