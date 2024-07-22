1d18012303d755c529475e0ff8c16a3eaa41e678
GANESH JAGGINENI <121171965+ganeshjaggineni@users.noreply.github.com>
2024-07-22 21:26:09 +0530

    Time: 31 ms (5.92%) | Memory: 45.1 MB (67.94%) - LeetSync

--- 2502-sort-the-people/sort-the-people.java
+++ 2502-sort-the-people/sort-the-people.java
@@ -3,7 +3,7 @@ class Solution {
         int n = heights.length;
         for(int i=0;i<n-1;i++)
         {
-            for(int j=0;j<n-1-i;j++)
+            for(int j=0;j<n-1;j++)
             {
                 if(heights[j] < heights[j+1])
                 {
