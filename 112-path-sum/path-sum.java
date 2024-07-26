4a8404b536b6677a3ee968cf2ee8ea478a3266ba
GANESH JAGGINENI <121171965+ganeshjaggineni@users.noreply.github.com>
2024-07-26 12:17:08 +0530

    Time: 0 ms (100.00%) | Memory: 42.8 MB (82.91%) - LeetSync

---
+++ 112-path-sum/path-sum.java
@@ -0,0 +1,32 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public boolean hasPathSum(TreeNode root, int targetSum) {
+
+        int sum = 0;
+        return hasPathSumHelper(root,sum,targetSum);
+        
+    }
+    public static boolean hasPathSumHelper(TreeNode root,int sum,int target)
+    {
+        if(root == null)
+            return false;
+        sum += root.val;
+       if(root.left == null && root.right == null)
+            return sum == target;
+        return hasPathSumHelper(root.left,sum,target) || hasPathSumHelper(root.right,sum,target);
+    }
+}
