f6503c3d13a9e3bd3bd9a8f06d7ddca8243d94a9
GANESH JAGGINENI <121171965+ganeshjaggineni@users.noreply.github.com>
2024-08-02 21:21:56 +0530

    Time: 36 ms (5.25%) | Memory: 44.6 MB (5.69%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.java
+++ 1013-fibonacci-number/fibonacci-number.java
@@ -1,9 +1,30 @@
 class Solution {
     public int fib(int n) {
+        //METHOD 1 : USING RECURSION
+        /*
         if(n==0 || n==1)
             return n;
 
         return fib(n-1)+fib(n-2);
+        */
+        //METHOD 2 USING DYNAMIC PROGRAMMING
+
+       return dpFib(n);
         
     }
+    
+    public static int dpFib(int n)
+    {
+    int dp[] = new int[n+1];
+    Arrays.fill(dp,-1);
+        if(n==0 || n==1)
+            return n;
+        if(dp[n] != -1)
+            return dp[n];
+        return dp[n] = dpFib(n-1) + dpFib(n-2);
+        
+    
+    }
+    //BY GANESH JAGGINENI
+
 }
