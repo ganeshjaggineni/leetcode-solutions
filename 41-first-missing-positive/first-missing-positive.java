f2c16330063e76a9a8b229a60f1714facfb0ad21
GANESH JAGGINENI <121171965+ganeshjaggineni@users.noreply.github.com>
2024-08-04 22:15:49 +0530

    Time: 3 ms (33.78%) | Memory: 56.8 MB (81.18%) - LeetSync

---
+++ 41-first-missing-positive/first-missing-positive.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int firstMissingPositive(int[] nums) {
+        
+        int n = nums.length;
+        System.out.println(n);
+        boolean[] visited = new boolean[n+1];
+        //0 indexed
+        for(int i=0;i<n;i++)
+        {
+            if(nums[i] > 0 && nums[i] <= n)
+                visited[nums[i]] = true;
+        }
+        for(int i=1;i<=n;i++)
+        {
+            if(!visited[i])
+                return i;
+        }
+        return n+1;
+    }
+}
