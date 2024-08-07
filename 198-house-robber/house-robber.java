1d8db56a0296f7b7a4bd3065e646e4ccff0ce370
GANESH JAGGINENI <121171965+ganeshjaggineni@users.noreply.github.com>
2024-08-07 23:57:47 +0530

    Time: 0 ms (100.00%) | Memory: 40.7 MB (70.47%) - LeetSync

--- 198-house-robber/house-robber.java
+++ 198-house-robber/house-robber.java
@@ -1,4 +1,6 @@
 class Solution {
+    /*
+            METHOD 1 : MEMOIZATION
     static int dp[];
     public int rob(int[] nums) {
         int i = 0;
@@ -16,5 +18,26 @@ class Solution {
         int skipHouse = robHelper(i+1,nums);
         return dp[i] = Math.max(robHouse,skipHouse);
     }
+    */
+
+    //METHOD 2 TABULATION
+      static int dp[];
+    public int rob(int[] nums) {
+        if(nums.length == 1)
+            return nums[0];
+        dp = new int[nums.length+1];
+        dp[0] = 0;
+        dp[1] = nums[0];
+        dp[2] = Math.max(nums[0],nums[1]);
+
+        for(int i=3;i<=nums.length;i++)
+        {
+            dp[i] = Math.max(dp[i-1],nums[i-1]+dp[i-2]);
+        }
+        return dp[nums.length];
+        
+        
+    }
+   
     //BY GANESH JAGGINENI
 }
