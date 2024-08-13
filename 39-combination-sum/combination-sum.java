3cdf27eee51fa9626fdc96ce15724705c2daf5a3
GANESH JAGGINENI <121171965+ganeshjaggineni@users.noreply.github.com>
2024-08-13 21:17:21 +0530

    Time: 2 ms (84.16%) | Memory: 44.6 MB (32.26%) - LeetSync

--- 39-combination-sum/combination-sum.java
+++ 39-combination-sum/combination-sum.java
@@ -2,23 +2,23 @@ class Solution {
         List<List<Integer>> mainList = new ArrayList<>();
     public List<List<Integer>> combinationSum(int[] candidates, int target) {
         
-        combinationSumHelper(candidates,0,0,target,new ArrayList<>());
-        return mainList;
+        combinationSumHelper(candidates,0,target,new ArrayList<>());
+      return mainList;
     }
-    public void combinationSumHelper(int[] candidates,int idx,int sum,int target,List<Integer> subList)
+    public void combinationSumHelper(int[] candidates,int idx,int target,List<Integer> subList)
     {
-        // List<Integer> subList = new ArrayList<>();
-        if(sum > target || idx >= candidates.length)
+        
+        if(idx >= candidates.length || target < 0)
             return ;
-        if(sum == target)
+        if(target == 0 )
         {
-           mainList.add(new ArrayList<>(subList)); // Add a copy of subList
+            mainList.add(new ArrayList<>(subList));
             return;
         }
         subList.add(candidates[idx]);
-        combinationSumHelper(candidates,idx,sum+candidates[idx],target,subList);
+        combinationSumHelper(candidates,idx,target-candidates[idx],subList);
         subList.remove(subList.size()-1);
-        combinationSumHelper(candidates,idx+1,sum,target,subList);
-
+        combinationSumHelper(candidates,idx+1,target,subList);
+            
     }
 }
