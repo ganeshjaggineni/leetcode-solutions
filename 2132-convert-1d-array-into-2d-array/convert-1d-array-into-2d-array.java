90cd6155fcb0b845bf528fcb460dfa0adf46a306
GANESH JAGGINENI <121171965+ganeshjaggineni@users.noreply.github.com>
2024-09-01 16:17:17 +0530

    Time: 3 ms (97.73%) | Memory: 55.9 MB (5.46%) - LeetSync

--- 2132-convert-1d-array-into-2d-array/convert-1d-array-into-2d-array.java
+++ 2132-convert-1d-array-into-2d-array/convert-1d-array-into-2d-array.java
@@ -1,6 +1,8 @@
 class Solution {
     public int[][] construct2DArray(int[] original, int m, int n) {
-        
+
+       //method 1 : Extreme stimulation
+       /* 
         int[][] res = new int[m][n];
         int row = 0;
         int currIdx = 0;
@@ -20,5 +22,20 @@ class Solution {
             row++;
         }
         return res;
+        */
+
+        //method 2 : simulation || logical iterating
+        if(m*n != original.length)
+            return new int[][]{};
+        int currIdx = 0;
+        int[][] resMat = new int[m][n];
+        for(int row = 0;row < m; row++)
+        {
+            for(int col = 0;col<n;col++)
+            {
+                resMat[row][col] = original[currIdx++];
+            }
+        }
+        return resMat;
     }
 }
