4c4877c94324eea5a12bd71bc0ee07718158e636
GANESH JAGGINENI <121171965+ganeshjaggineni@users.noreply.github.com>
2024-09-03 14:11:12 +0530

    Time: 0 ms (100.00%) | Memory: 41.7 MB (71.17%) - LeetSync

--- 2076-sum-of-digits-of-string-after-convert/sum-of-digits-of-string-after-convert.java
+++ 2076-sum-of-digits-of-string-after-convert/sum-of-digits-of-string-after-convert.java
@@ -2,26 +2,23 @@ class Solution {
     public int getLucky(String s, int k) {
         
         int sum = 0;
-
         for(int i=0;i<s.length();i++)
         {
-            System.out.println("char "+(s.charAt(i)-96));
            sum += findSum(s.charAt(i)-96);
-           System.out.println("sum = "+sum);
         }
         k = k-1;
            
-    while(k-- > 0)
-    {
-         int num = sum;
-        sum = 0;
-        while(num != 0)
+        while(k-- > 0)
         {
-            sum += num%10;
-            num /= 10;
+            int num = sum;
+            sum = 0;
+            while(num != 0)
+            {
+                sum += num%10;
+                num /= 10;
+            }
         }
-    }
-    return sum;
+         return sum;
     }
     public int findSum(int num)
     {
